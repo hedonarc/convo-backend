b11917deb1a6adcf02b9874667a22c1b275ed8a5
Abubakar Khawaja <abubakark1998@gmail.com>
2026-05-27 03:56:20 +0500

    fix: initialize asgi before everything

--- config/asgi.py
+++ config/asgi.py
@@ -1,17 +1,18 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
+
 from django.core.asgi import get_asgi_application
 
-from apps.authentication.middleware import JWTAuthMiddlewareStack
-from apps.conversations.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from apps.authentication.middleware import JWTAuthMiddlewareStack
+from apps.conversations.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
